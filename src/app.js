189c8cf38fdafe9cdf250b3450cba6bb39c8c0d7
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2025-02-16 12:29:05 +0200

    - Added Auth controller and service

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { loadEnv } = require('./shared/config/loadEnv');
 const { loggerMiddleware } = require('./shared/middleware/logger.middleware');
-const { userRouter } = require('./users/users.routes');
+const { userRouter } = require('./features/users/users.routes');
+const { authRouter } = require('./features/auth/auth.routes');
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(loggerMiddleware);
 
 app.use('/users', userRouter);
+
+app.use('/auth', authRouter);
 
 
 
