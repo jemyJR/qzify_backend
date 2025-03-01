0f083932450840ed0f4b392d951eecb7174db5bc
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2025-03-01 23:58:54 +0200

    - Refactor users services to use funnctions not class

--- src/features/users/users.service.js
+++ src/features/users/users.service.js
@@ -2,55 +2,48 @@ const User = require('./user.model');
 const bcrypt = require('bcrypt');
 const { ResourceNotFoundError, RuntimeError } = require('../../shared/utils/errorTypes');
 
-class UserService {
-    static async getUsers() {
-        const users = await User.find().select('-password');
-        return users;
+exports.getUsers = async function() {
+    const users = await User.find().select('-password');
+    return users;
+};
+
+exports.getUserById = async function(id) {
+    const user = await User.findById(id).select('-password');
+    if (!user) {
+        throw new ResourceNotFoundError('User', 'id', id, 'User not found');
     }
+    return user;
+};
 
-    static async getUserById(id) {
-        const user =await User.findById(id).select('-password');
-        if (!user) {
-            throw new ResourceNotFoundError('User', 'id', id, 'User not found');
-        }
-        return user;
+exports.createUser = async function(newUser) {
+    const existingUser = await User.findOne({ email: newUser.email });
+    if (existingUser) {
+        throw new RuntimeError('User already exists', 'User with this email already exists', 400);
     }
+    const hashedPassword = await bcrypt.hash(newUser.password, 10);
+    newUser.password = hashedPassword;
+    const user = new User(newUser);
+    await user.save();
+    const { password, ...userWithoutPassword } = user.toObject();
+    return userWithoutPassword;
+};
 
-    static async createUser(newUser) {
-        const existingUser = await User.findOne({ email: newUser.email });
-        if (existingUser) {
-            throw new RuntimeError('User already exists', 'User with this email already exists', 400);
-        }
-        const hashedPassword = await bcrypt.hash(newUser.password, 10);
-        newUser.password = hashedPassword;
-        const user = new User(newUser);
-        await user.save();
-        const { password, ...userWithoutPassword } = user.toObject();
-        return userWithoutPassword;
-
+exports.updateUser = async function(id, updatedUser) {
+    if (updatedUser.hasOwnProperty('role')) {
+        delete updatedUser.role;
     }
-
-    static async updateUser(id, updatedUser) {
-        
-        if (updatedUser.hasOwnProperty('role')) {
-            delete updatedUser.role;
-        }
-        
-        const user =await User.findByIdAndUpdate(id, updatedUser, { new: true }).select('-password');
-        if (!user) {
-            throw new ResourceNotFoundError('User', 'id', id, 'User not found');
-        }
-        return user;
+    
+    const user = await User.findByIdAndUpdate(id, updatedUser, { new: true }).select('-password');
+    if (!user) {
+        throw new ResourceNotFoundError('User', 'id', id, 'User not found');
     }
+    return user;
+};
 
-    static async deleteUser(id) {
-        const user = await User.findByIdAndDelete(id);
-        if (!user) {
-            throw new ResourceNotFoundError('User', 'id', id, 'User not found');
-        }
-        return { message: 'User deleted successfully' };
+exports.deleteUser = async function(id) {
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+        throw new ResourceNotFoundError('User', 'id', id, 'User not found');
     }
-}
-
-module.exports = UserService;
-
+    return { message: 'User deleted successfully' };
+};
