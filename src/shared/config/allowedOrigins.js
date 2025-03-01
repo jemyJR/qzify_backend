82c3f5ac145ccc0a3a626b2e52cbd04dbbc1baa0
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2025-03-01 19:58:13 +0200

    Added Security Middleware  ✅

--- src/shared/config/allowedOrigins.js
+++ src/shared/config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    process.env.API_URL,
 ];
 
 module.exports = allowedOrigins;
--- src/shared/config/corsOrigins.js
+++ src/shared/config/corsOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = require('./allowedOrigins');
 
 const coreOptions = {
     origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
