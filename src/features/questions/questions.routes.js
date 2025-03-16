d258a763b5c070f74c6676149fc8afbe9db59410
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2025-03-16 19:53:26 +0200

    - Fix verifyJWT middleware problem

--- src/features/questions/questions.routes.js
+++ src/features/questions/questions.routes.js
@@ -2,10 +2,16 @@ const express = require('express');
 const QuestionsController = require('./questions.controller');
 const { validate } = require('../../shared/utils/validate');
 const { questionValidator } = require('./question.validator');
+const { verifyJWT } = require('../../shared/middleware/verifyJWT.middleware');
+const { requireAdmin } = require('../../shared/middleware/requireAdmin.middleware');
 
 const questionsRouter = express.Router();
 
+questionsRouter.use(verifyJWT);
+
 questionsRouter.get('/category', QuestionsController.getAllCategories);
+
+questionsRouter.use(requireAdmin);
 
 questionsRouter.get('/category/:category', QuestionsController.getQuestionsByCategory);
 
--- src/shared/middleware/verifyJWT.middleware.js
+++ src/shared/middleware/verifyJWT.middleware.js
@@ -5,23 +5,26 @@ const { RuntimeError } = require('../utils/errorTypes');
 const verifyJWT = async (req, res, next) => {
     const authHeader = req.headers['authorization'];
     if (!authHeader?.startsWith('Bearer ')) {
-        throw new RuntimeError('Unauthorized', 'No token provided', 401);
+        return next(new RuntimeError('Unauthorized', 'No token provided', 401)); 
     }
+    
     const token = authHeader.split(' ')[1];
     try {
         const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         const user = await userService.getUserById(decoded.id);
+        
         if (!user) {
-            throw new RuntimeError('Unauthorized', 'User not found', 401);
+            return next(new RuntimeError('Unauthorized', 'User not found', 401));
         }
-
+        
         if (user.tokenVersion !== decoded.tokenVersion) {
-            throw new RuntimeError("Session expired", "Please log in again.", 401);
+            return next(new RuntimeError("Session expired", "Please log in again.", 401));
         }
+        
         req.user = decoded;
         next();
     } catch (err) {
-        throw new RuntimeError('Unauthorized', 'Invalid token', 401);
+        next(err);
     }
 }
 
