e94edebaa682bb7ea2e0725e018570bec2bf4bf1
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2025-03-25 11:54:36 +0200

    - Added userWithoutSensitiveInfo helper fuction

--- src/features/users/user.utils.js
+++ src/features/users/user.utils.js
@@ -1,7 +1,11 @@
-const removePassword = (user) => {
+exports.removePassword = function (user) {
     if (!user) return null;
     const { password, ...userWithoutPassword } = user.toObject();
     return userWithoutPassword;
 };
 
-module.exports = { removePassword };
+exports.removeSensitiveInfo = function (user) {
+    if (!user) return null;
+    const { password, tokenVersion, resetPassToken, resetPassTokenExpire, isVerified, verificationToken, verificationTokenExpire, ...userWithoutSensitiveInfo } = user.toObject();
+    return userWithoutSensitiveInfo;
+};
